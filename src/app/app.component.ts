0e0be258c394c8ae01aa8ad28453d6b7e1bf93f8
Alexandre de Oliveira <alexmontanha@hotmail.com>
2022-09-15 07:51:38 -0300

    Adicionando entries ao loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,12 +23,21 @@ export class AppComponent {
     this.clienteService.getClientes().subscribe(data => {
       const ids = Object.keys(data);
       const values = Object.values(data);
+      const entries = Object.entries(data);
+
       ids.forEach(id => {
         console.log(id);
       });
+
       values.forEach(value => {
         console.log(value);
       });
+
+      entries.forEach(entrie => {
+        console.log(entrie);
+      });
+
+
     });
   }
 
